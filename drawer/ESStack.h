a87b9b0930d4838843fc45650628b797ac4f3694
Anvar Eldarov <elanserr@gmail.com>
2016-09-16 22:12:06 +0500

    add ESStack

---
+++ drawer/ESStack.h
@@ -0,0 +1,13 @@
+//
+//  ESStack.h
+//  drawer
+//
+//  Created by Anvar Eldarov on 16.09.16.
+//  Copyright © 2016 Anvar Eldarov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ESStack : NSObject
+
+@end

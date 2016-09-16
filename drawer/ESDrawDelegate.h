dc1ff47d084ea761340ac5e177dc954d29288026
Anvar Eldarov <elanserr@gmail.com>
2016-09-16 21:53:15 +0500

    add ESDrawDelegate

---
+++ drawer/ESDrawDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ESDrawDelegate.h
+//  drawer
+//
+//  Created by Anvar Eldarov on 16.09.16.
+//  Copyright © 2016 Anvar Eldarov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ESDrawDelegate : NSObject
+
+@end
